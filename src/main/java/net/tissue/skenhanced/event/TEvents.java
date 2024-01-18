2126038351320ff2c78bb925b03ab7913e46be21
Captain1444 <39641058+Captain1444@users.noreply.github.com>
2024-01-17 23:20:33 -0600

    Add files via upload

---
+++ src/main/java/net/tissue/skenhanced/event/TEvents.java
@@ -0,0 +1,16 @@
+package net.tissue.skenhanced.event;
+
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.tissue.skenhanced.SkEnhanced;
+import net.minecraftforge.fml.common.Mod;
+import net.tissue.skenhanced.entity.skeletons.IceSpikesSkeleton;
+import net.tissue.skenhanced.init.TEntities;
+
+@Mod.EventBusSubscriber(modid = SkEnhanced.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TEvents {
+    @SubscribeEvent
+    public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
+        event.put(TEntities.ICE_SPIKE_SKELETON.get(), IceSpikesSkeleton.setAttributes());
+    }
+}
